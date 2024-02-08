002a97c18088a8f1daa823b98068675f1d9086b1
Priyanka25062004 <159120049+Priyanka25062004@users.noreply.github.com>
2024-02-08 11:59:31 +0530

    Add files via upload

---
+++ Program to display given year is leap year or not.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter year";
+cin>>n;
+if(n%4==0)
+cout<<"given year is a leap year";
+else
+cout<<"given year is not a leap year";
+return 0;
+}

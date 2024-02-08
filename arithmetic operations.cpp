35bbd05b9d2ceea85a11626fb361bbe6a4d350f0
Priyanka25062004 <159120049+Priyanka25062004@users.noreply.github.com>
2024-02-08 12:02:00 +0530

    Add files via upload

---
+++ arithmetic operations.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cout<<"enter two numbers";
+	cin>>num1>>num2;
+	cout<<"\n sum is"<<num1+num2;
+	cout<<"\n diff is"<<num1-num2;
+	cout<<"\n product is"<<num1*num2;
+	cout<<"\n quotient is"<<num1/num2;
+	cout<<"\n remainder is"<<num1%num2;
+	return 0;
+}
